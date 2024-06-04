e06ac3fa98df7ea9e78c618e5ce991aaddffd261
sonivishal238 <68811345+sonivishal238@users.noreply.github.com>
2024-06-04 14:46:49 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,13 @@
-def call() {
-    bat 'echo Hi from the shared pipeline'
+def printRepoName(String repoName) {
+  // Use Jenkins pipeline syntax to print the name within a stage
+  pipeline {
+    agent none // Define no agent as we're not building anything
+    stages {
+      stage('Print Repo Name') {
+        steps {
+          echo "Building microservice: ${repoName} from shared pipeline"
+        }
+      }
+    }
+  }
 }
